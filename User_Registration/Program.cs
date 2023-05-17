0a7a5529e5049ea843a1fdaca3bffd013211e48c
Harshita <chandaliyaharshita222@gmail.com>
2023-05-17 21:54:37 +0530

    Valid User Last Name

--- User_Registration/Program.cs
+++ User_Registration/Program.cs
@@ -8,11 +8,11 @@ namespace User_Registration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Problem using REGEX and Pattern!");
-            Console.WriteLine("Valid User First Name!");
-            string name = "[A-Z][a-z]{3,}";
+            Console.WriteLine("Valid User Last Name!");
+            string name = "[A-Z][a-z]{3,}[ ][A-Z][a-z]{3,}";
 
             Regex regex = new Regex(name);
-            Console.WriteLine("Enter first name:");
+            Console.WriteLine("Enter first and last name:");
             string s = Console.ReadLine();
             Console.WriteLine("It is: " + regex.IsMatch(s));
         }
