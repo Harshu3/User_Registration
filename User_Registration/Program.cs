59bd9085e9d667671b23801c8c12a4816a439236
Harshita <chandaliyaharshita222@gmail.com>
2023-05-27 23:28:34 +0530

    Valid email address

--- User_Registration/Program.cs
+++ User_Registration/Program.cs
@@ -8,11 +8,11 @@ namespace User_Registration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Problem using REGEX and Pattern!");
-            Console.WriteLine("Valid User Last Name!");
-            string name = "[A-Z][a-z]{3,}[ ][A-Z][a-z]{3,}";
+            Console.WriteLine("Valid User Email ID!");
+            string email = @"[a-z]{3,}.?[a-z0-9]{3,}[@][a-z]{2,}.(com|org)$";
 
-            Regex regex = new Regex(name);
-            Console.WriteLine("Enter first and last name:");
+            Regex regex = new Regex(email);
+            Console.WriteLine("Enter Email ID:");
             string s = Console.ReadLine();
             Console.WriteLine("It is: " + regex.IsMatch(s));
         }
