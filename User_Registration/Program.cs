bfe5745db44c27ae2d33e20b137902093e278dd2
Harshita <chandaliyaharshita222@gmail.com>
2023-05-28 19:14:21 +0530

    Pre-defined mobile format

--- User_Registration/Program.cs
+++ User_Registration/Program.cs
@@ -8,11 +8,11 @@ namespace User_Registration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Problem using REGEX and Pattern!");
-            Console.WriteLine("Valid User Email ID!");
-            string email = @"[a-z]{3,}.?[a-z0-9]{3,}[@][a-z]{2,}.(com|org)$";
+            Console.WriteLine("User need to follow pre-defined Mobile Format!");
+            string phoneNumber = @"^[0-9]{2}[ ][0-9]{10}$";
 
-            Regex regex = new Regex(email);
-            Console.WriteLine("Enter Email ID:");
+            Regex regex = new Regex(phoneNumber);
+            Console.WriteLine("Enter Phone Number:");
             string s = Console.ReadLine();
             Console.WriteLine("It is: " + regex.IsMatch(s));
         }
