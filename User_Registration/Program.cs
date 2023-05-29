16ec024901e144e9277c1ee331ed109f2c6296fa
Harshita <chandaliyaharshita222@gmail.com>
2023-05-29 19:48:39 +0530

    Pre-defined_PassWord_Rule-3

--- User_Registration/Program.cs
+++ User_Registration/Program.cs
@@ -9,8 +9,8 @@ namespace User_Registration
         {
             Console.WriteLine("Welcome to User Registration Problem using REGEX and Pattern!");
             Console.WriteLine("User need to follow Pre-defined PassWord rules!");
-            Console.WriteLine("[Rule 2] Should have at least 1 Upper Case");
-            string passWord = @"^(?=.*?[A-Za-z]).{8,}$";
+            Console.WriteLine("[Rule 3] Should have at least 1 numeric number");
+            string passWord = @"^(?=.*?[A-Za-z0-9]).{8,}$";
 
             Regex regex = new Regex(passWord);
             Console.WriteLine("Enter PassWord:");
