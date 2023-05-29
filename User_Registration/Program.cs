18ce45dece5d0e7a6bd64f3fdc837124e83c70b8
Harshita <chandaliyaharshita222@gmail.com>
2023-05-29 18:45:17 +0530

    Valid user first name

--- User_Registration/Program.cs
+++ User_Registration/Program.cs
@@ -9,7 +9,7 @@ namespace User_Registration
         {
             Console.WriteLine("Welcome to User Registration Problem using REGEX and Pattern!");
             Console.WriteLine("Valid User First Name!");
-            string name = "^[A-Z][a-z]{3,}$";
+            string name = "^[A-Za-z]{3,}$";
 
             Regex regex = new Regex(name);
             Console.WriteLine("Enter first name:");
